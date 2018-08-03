7d4b236f4ea17fbe831e869afba9b39df1e84e76
jeffrey <jeffrey.fisher@anu.edu.au>
2018-08-03 17:29:28 +1000

    Updated for changes to TMNS.

--- Entities/Furnishings/FurnishingDetails.cs
+++ Entities/Furnishings/FurnishingDetails.cs
@@ -1,17 +1,28 @@
-﻿// Tidied for version 0.3.
-
 using System;
 using System.Collections.Generic;
 
-namespace RLEngine.Entities.Furnishings
+namespace TMNS.Entities.Furnishings
 {
 	[Serializable]
 	public class FurnishingDetails:EntityBasicDetails
 	{
-		public FurnishingDetails(string entityName, char symbol, string fgColorName, List<Trait> traits)
-			: base(entityName, symbol, fgColorName, traits) { }
+		public string Description;
+		public Material Material;
 
-		public FurnishingDetails(string entityName, char symbol, string fgColorname, Trait[] traits)
-			: base(entityName, symbol, fgColorname, traits) { }
+		public FurnishingDetails(string entityName, char symbol, string fgColorName, string description,
+		                         Material material, List<Trait> traits)
+			: base(entityName, symbol, fgColorName, traits) 
+		{
+			Description = description;
+			Material = material;
+		}
+
+		public FurnishingDetails(string entityName, char symbol, string fgColorname, string description,
+		                         Material material, Trait[] traits)
+			: base(entityName, symbol, fgColorname, traits) 
+		{
+			Description = description;
+			Material = material;
+		}
 	}
 }
