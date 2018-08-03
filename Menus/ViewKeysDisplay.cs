3cd7a5a2c5028d9494f1327ca66618608c5b26ad
jeffrey <jeffrey.fisher@anu.edu.au>
2018-08-03 13:19:55 +1000

    Did initial tidy up after conversion to TMNS - Added some additional keys. Still needs more work. Also still needs Extra Keys sorted out.

--- Menus/ViewKeysDisplay.cs
+++ Menus/ViewKeysDisplay.cs
@@ -1,13 +1,12 @@
-// Tidied up for version 0.2 - no changes for version 0.3.
-
 using System.Collections.Generic;
 
-namespace RLEngine.Menus
+namespace TMNS.Menus
 {
 	public class ViewKeysDisplay
 	{
 		readonly List<string> _keys = new List<string>
 		{
+			"A:  Acquire an item on the ground",
 			"B:  Display the contents of your backpack",
 			"C:  Display the character menu",
 			"E:  Examine an object or creature",
@@ -15,14 +14,18 @@ namespace RLEngine.Menus
 			"I:  Use an item from your inventory",
 			"J:  Display your quest journal",
 			"K:  Display this list of commands",
+			"P:  Kneel in Prayer or Pray at an altar",
 			"S:  Search your surroundings",
 			"T:  Talk to a creature or NPC",
 			"U:  Use an object such as a door or lever",
 			"V:  Display the achievement menu",
+			"Y:  Use an ability or cast a spell",
 			"",
 			"Spacebar:  Pass your current turn",
-			"Direction Keys:  Move in the given direction"
+			"Direction Keys:  Move or attack in the given direction"
 		};
+
+		// TODO: Add extra keys if present.
 
 		public void ViewKeys()
 		{
