6e2782276d33ed228af8e449303ec6b249567271
jeffrey <jeffrey.fisher@anu.edu.au>
2018-08-03 15:34:10 +1000

    Updated for the required tile types for level 1 and 2.

--- Entities/MapTiles/TileType.cs
+++ Entities/MapTiles/TileType.cs
@@ -1,12 +1,13 @@
-﻿// Finished for version 0.1 - no change for 0.3.
-
-namespace RLEngine.Entities.MapTiles
+namespace TMNS.Entities.MapTiles
 {
 	public enum TileType
 	{
-		TestTile1,
-		TestTile2,
-		TestTile3, // Doesn't exist, used to test errors/defaults.
-
+		StoneFloor,
+		Wall,
+		Space,
+		Water,
+		Icy,
+		IcyLedge,
+		Grease
 	}
 }
