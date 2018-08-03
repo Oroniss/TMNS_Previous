f1ad5262d9c043dd6376c0afdbc68f442b79b55c
jeffrey <jeffrey.fisher@anu.edu.au>
2018-08-04 00:36:58 +1000

    Updated for TMNS - needs interaction traps sorted out properly.

--- Entities/Furnishings/InteractionFunctions.cs
+++ Entities/Furnishings/InteractionFunctions.cs
@@ -1,14 +1,13 @@
-﻿using System.Collections.Generic;
-using RLEngine.Entities.Actors;
+using System.Collections.Generic;
+using TMNS.Entities.Actors;
 using System;
 
-namespace RLEngine.Entities.Furnishings
+namespace TMNS.Entities.Furnishings
 {
 	public partial class Furnishing
 	{
 		static Dictionary<string, InteractionFunction> interactionFunctions = new Dictionary<string, InteractionFunction>
 		{
-			{"TestInteractionFunction1", TestInteractionFunction1},
 			{"LevelTransitionInteraction", LevelTransitionInteraction},
 
 			// Interaction Traps
@@ -17,14 +16,6 @@ namespace RLEngine.Entities.Furnishings
 		};
 
 		delegate void InteractionFunction(Furnishing furnishing, Actor actor);
-
-		static void TestInteractionFunction1(Furnishing furnishing, Actor actor)
-		{
-			if (furnishing.HasTrait(Trait.BlockMove))
-				furnishing.RemoveTrait(Trait.BlockMove);
-			else
-				furnishing.AddTrait(Trait.BlockMove);
-		}
 
 		static void LevelTransitionInteraction(Furnishing furnishing, Actor actor)
 		{
